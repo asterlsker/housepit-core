3fd66d51d1e467cd4b5bb3832242b70549faad68
ujon <dkcushow@gmail.com>
2022-10-31 11:33:01 +0900

    feat: update Authentication annotation
    modify java.lang.annotation to kotlin.annotation

--- src/main/kotlin/com/asterlsker/housepit/core/annotation/Authentication.kt
+++ src/main/kotlin/com/asterlsker/housepit/core/annotation/Authentication.kt
@@ -1,13 +1,11 @@
 package com.asterlsker.housepit.core.annotation
 
 import com.asterlsker.housepit.core.enums.AuthenticationScope
-import java.lang.annotation.*
-import java.lang.annotation.Retention
-import java.lang.annotation.Target
+import kotlin.annotation.Retention
+import kotlin.annotation.Target
 
-@Documented
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class Authentication(
     val scope: AuthenticationScope = AuthenticationScope.REQUIRED
 )
